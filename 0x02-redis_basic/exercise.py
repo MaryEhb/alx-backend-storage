ad5ebd0796d12ea16c4d7f94a963b6460ce880ab
Mariem <mariem.ehab234@gmail.com>
2024-06-19 15:47:26 -0700

    Task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,10 +9,15 @@ class Cache:
     '''create redis store for caching'''
 
     def __init__(self):
+        '''store an instance of the Redis client as a private variable
+        named _redis and flush it'''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, int, bytes, float]) -> str:
+        '''takes a data argument and returns a string. The method should
+        generate a random key (e.g. using uuid), store the input data
+        in Redis using the random key and return the key.'''
         key = str(uuid4())
         self._redis.set(key, data)
         return key
