6ad9e7c02b83619fedf0550079c3776eee5623a9
Mariem <mariem.ehab234@gmail.com>
2024-06-19 16:43:44 -0700

    Task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -29,7 +29,7 @@ def call_history(method: Callable) -> Callable:
 
 
 def replay(fn: Callable) -> None:
-    ''' '''
+    '''function to display the history of calls of a particular function.'''
     client = redis.Redis()
     calls = client.get(fn.__qualname__).decode('utf-8')
     inputs = [input.decode('utf-8') for input in
